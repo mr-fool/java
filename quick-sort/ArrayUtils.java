0e4f630a3c6e0dfc8abfecd15bb093e9aa8470da
mr-fool <mr.law.jason@gmail.com>
2014-12-27 20:35:46 +0800

    Create ArrayUtils.java

---
+++ quick-sort/ArrayUtils.java
@@ -0,0 +1,31 @@
+package cpsc331.assignment4;
+
+import cpsc331.collections.Array;
+
+public class ArrayUtils<T extends Comparable<T>> {
+
+  public void dQuickSort1( Array<T> A ){
+
+    // This implementation must be changed
+
+  };
+  
+    public void rQuickSort1( Array<T> A ){
+
+    // This implementation must be changed
+
+  };
+  
+    public void dQuickSort2( Array<T> A ){
+
+    // This implementation must be changed
+
+  };
+  
+    public void rQuickSort2( Array<T> A ){
+
+    // This implementation must be changed
+
+  };
+
+}
