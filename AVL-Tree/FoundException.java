cf39d822d176867c03c94a105702df378200e739
mr-fool <mr.law.jason@gmail.com>
2014-12-27 19:50:01 +0800

    Create FoundException.java

---
+++ AVL-Tree/FoundException.java
@@ -0,0 +1,24 @@
+package cpsc331.assignment3;
+
+class FoundException extends RuntimeException {
+
+/*
+ * Constructs a FoundException
+ *
+ */
+
+  public FoundException() {
+    super();
+  }
+
+ /*
+ *
+ * Constructs a FoundException with the specified message.
+ *
+ */
+
+  public FoundException(String message) {
+    super(message);
+  };
+
+}
