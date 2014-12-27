2083a19a8ae922c2a8cf0bc29d4c8d7f6abbe437
mr-fool <mr.law.jason@gmail.com>
2014-12-27 20:44:50 +0800

    Create try.java

---
+++ proxychain/try.java
@@ -0,0 +1,48 @@
+import java.net.Socket;
+import java.util.Scanner;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+/* Socket does not like HTTP proxy, SOCKS sometimes work */
+
+
+/**
+ * @author mr-fool
+ * @version beta
+ * @param proxychain attempt*/
+ 
+public class ProxyChain {
+	public static void main (String[] args) {
+		//Prompting for ip address
+		System.out.print("Please enter an valid ip address: ");
+		Scanner address = new Scanner(System.in);
+		String serverAddress = address.nextLine();
+		//Removing whitespace
+		serverAddress = serverAddress.replace(" ","");
+		
+		System.out.print("Please enter an valid port: ");
+		Scanner port = new Scanner(System.in);
+		int serverPort = port.nextInt();
+		
+		System.out.print("Please enter the proxy type Proxy.Type.SOCKS or Proxy.Type.HTTP or Proxy.Type.DIRECT");
+		Scanner type = new Scanner(System.in);
+		String proxyType = type.nextLine();
+		//Removing whitespace
+		proxyType = proxyType.replace(" ","");
+		
+		
+		try {
+			System.out.println ("Attempting to connect " + proxyType + ":" + serverAddress + ":" + serverPort); 
+			Socket connection = new Socket(new Proxy(Proxy.Type.valueOf(proxyType), new InetSocketAddress(serverAddress, serverPort)));
+                        System.out.println("Connection status: " + connection.isConnected() );
+
+		}
+		catch (IllegalArgumentException e) {
+			System.err.println("proxy is of an invalid type or null");
+		}
+		catch (SecurityException e) {
+			System.err.println("security manager is present and permission to connect to the proxy is denied");
+		}
+	}
+ }
+ 
