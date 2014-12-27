08a7a8aa7b426ec10f41e00304309e16dc9f8c53
mr-fool <mr.law.jason@gmail.com>
2014-12-27 19:50:34 +0800

    Create NotFoundException.java

---
+++ AVL-Tree/NotFoundException.java
@@ -0,0 +1,25 @@
+package cpsc331.assignment3;
+
+class NotFoundException extends RuntimeException {
+
+ /*
+ *
+ * Constructs a NotFoundException
+ *
+ */
+
+  NotFoundException() {
+    super();
+  }
+
+ /*
+ *
+ * Constructs a NotFoundException with the specified message.
+ *
+ */
+
+  NotFoundException(String message) {
+    super(message);
+  };
+
+}
