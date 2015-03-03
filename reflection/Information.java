8c6dfd11c516c58c093eff05fe68a40d7cc26300
mr-fool <mr-fool@users.noreply.github.com>
2015-03-02 23:40:08 -0700

    Update Information.java

--- reflection/Information.java
+++ reflection/Information.java
@@ -1,45 +1,50 @@
 import java.util.*;   
 import java.lang.reflect.*;   
+       
 
-public class Information {
-	public static void main(String[] args) { 
-	/*read the project prefix in 
-	* This part will be hardcored after the testing is completed
-	*/
-	ArrayList<String> method = new ArrayList<String>(); 
-	ArrayList<String> returnType = new ArrayList<String>(); 
-		String name;   
-		if (args.length > 0)
-			name = args[0];   
-		else  {   
-			Scanner in = new Scanner(System.in);   
-			System.out.println("Enter my.project.prefix ");   
-			name = in.next();   
-		}
-		try {
-			Class cl = Class.forName(name);  
-			System.out.println("The project prefix: " + name);
-			method = methodName(cl);
-			for(String tmp: method) { 
-				System.out.println("List of methods " + method);
-			}
-		}
-		catch (ClassNotFoundException e)  {   
-			e.printStackTrace();   
-		}
-		catch (Exception e) {
-			e.printStackTrace();  
-		}  
-		System.exit(0);
-	}//end main
-	public static ArrayList<String> methodName(Class cl)   {  
-		ArrayList<String> method = new ArrayList<String>(); 
+public class Information   {   
+	public static void main(String[] args) {   
+    // read class name from command line args and if args is empty prompt user input   
+    String name;   
+    if (args.length > 0)
+		name = args[0];   
+    else  {   
+		Scanner in = new Scanner(System.in);   
+        System.out.println("Enter your project prefix ");   
+        name = in.next();   
+    }   
+       
+    try  {   
+		// print class name and superclass name (if != Object)   
+		Class cl = Class.forName(name);   
+		System.out.println("The project prefix: " + name);
+		printMethods(cl);   
+   
+	}   
+    catch (ClassNotFoundException e)  {   
+		e.printStackTrace();   
+    } 
+    catch (Exception e) {
+		e.printStackTrace();  
+	}  
+     System.exit(0);   
+}//end of main   
+ 
+	public static void printMethods(Class cl)   {   
 		Method[] methods = cl.getDeclaredMethods();   
 		for (Method m : methods)   {   
 			Class retType = m.getReturnType();   
             String name = m.getName();   
-            method.add(name);
-        }   
-       return method;
-       } //end of method
-}
+			System.out.println("The name of the method " + name);
+  
+       
+            // print parameter types   
+            Class[] paramTypes = m.getParameterTypes();   
+            for (int j = 0; j < paramTypes.length; j++)   {    
+                System.out.println(paramTypes[j].getName());   
+             }   
+            
+          }   
+       }//end of method   
+  
+    }
