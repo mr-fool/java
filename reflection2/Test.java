460aa0519d61e4eaf0a808361904ae21d50a09ad
mr-fool <mr-fool@users.noreply.github.com>
2015-03-09 19:29:00 -0600

    Update Test.java

--- reflection2/Test.java
+++ reflection2/Test.java
@@ -1,13 +1,17 @@
 public class Test {
 	public static void main(String[] args) throws Exception{
-		Information information = new Information("/home/mr-fool/Documents/JFLAP.jar","org/apache/batik/bridge/PaintBridge.class");
-		information.printAllFunction("org/apache/batik/bridge/PaintBridge.class");
+		Information information = new Information("/home/mr-fool/Documents/JFLAP.jar","org/apache/batik/bridge/PaintBridge");
+		information.printAllFunction("org/apache/batik/bridge/PaintBridge");
 	}//end main
 }
 /*
-java Test
-java.lang.ClassNotFoundException: org/apache/batik/bridge/PaintBridge.class
-	at java.net.URLClassLoader$1.run(URLClassLoader.java:372)
+java.lang.NoClassDefFoundError: IllegalName: org/apache/batik/bridge/PaintBridge
+	at java.lang.ClassLoader.preDefineClass(ClassLoader.java:654)
+	at java.lang.ClassLoader.defineClass(ClassLoader.java:758)
+	at java.security.SecureClassLoader.defineClass(SecureClassLoader.java:142)
+	at java.net.URLClassLoader.defineClass(URLClassLoader.java:455)
+	at java.net.URLClassLoader.access$100(URLClassLoader.java:73)
+	at java.net.URLClassLoader$1.run(URLClassLoader.java:367)
 	at java.net.URLClassLoader$1.run(URLClassLoader.java:361)
 	at java.security.AccessController.doPrivileged(Native Method)
 	at java.net.URLClassLoader.findClass(URLClassLoader.java:360)
@@ -15,10 +19,4 @@ java.lang.ClassNotFoundException: org/apache/batik/bridge/PaintBridge.class
 	at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
 	at Information.<init>(Information.java:17)
 	at Test.main(Test.java:3)
-Exception in thread "main" java.lang.ClassNotFoundException: org/apache/batik/bridge/PaintBridge.class
-	at java.lang.Class.forName0(Native Method)
-	at java.lang.Class.forName(Class.java:260)
-	at Information.printAllFunction(Information.java:38)
-	at Test.main(Test.java:4)
-
 */
