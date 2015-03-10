3a49711a0a259f239d3ed1bb7c2fe5736264b05b
mr-fool <mr-fool@users.noreply.github.com>
2015-03-09 20:31:34 -0600

    Delete Test.java

--- reflection2/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-	public static void main(String[] args) throws Exception{
-		Information information = new Information("/home/mr-fool/Documents/JFLAP.jar","org.apache.batik.bridge.PaintBridge");
-		information.printAllFunction(Information.cls);
-	}//end main
-}
