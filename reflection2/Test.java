2812e9f513b64c97fc7b0fc01e5416b2f647d01b
mr-fool <mr-fool@users.noreply.github.com>
2015-03-09 19:11:32 -0600

    Update Test.java

--- reflection2/Test.java
+++ reflection2/Test.java
@@ -5,6 +5,26 @@ public class Test {
 	}//end main
 }
 /*
-java Test.java
-Error: Could not find or load main class Test.java
+java.lang.ClassNotFoundException: JFLAP.class
+	at java.net.URLClassLoader$1.run(URLClassLoader.java:372)
+	at java.net.URLClassLoader$1.run(URLClassLoader.java:361)
+	at java.security.AccessController.doPrivileged(Native Method)
+	at java.net.URLClassLoader.findClass(URLClassLoader.java:360)
+	at java.lang.ClassLoader.loadClass(ClassLoader.java:424)
+	at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
+	at Information.<init>(Information.java:17)
+	at Test.main(Test.java:3)
+Exception in thread "main" java.lang.ClassNotFoundException: JFLAP.class
+	at java.net.URLClassLoader$1.run(URLClassLoader.java:372)
+	at java.net.URLClassLoader$1.run(URLClassLoader.java:361)
+	at java.security.AccessController.doPrivileged(Native Method)
+	at java.net.URLClassLoader.findClass(URLClassLoader.java:360)
+	at java.lang.ClassLoader.loadClass(ClassLoader.java:424)
+	at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:308)
+	at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
+	at java.lang.Class.forName0(Native Method)
+	at java.lang.Class.forName(Class.java:260)
+	at Information.printAllFunction(Information.java:38)
+	at Test.main(Test.java:4)
+
 */
