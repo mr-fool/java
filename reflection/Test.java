6203c0a5fe8500a280279bc9bee5aedeb1c570bf
mr-fool <mr-fool@users.noreply.github.com>
2015-03-14 23:37:59 -0600

    Update Test.java

--- reflection/Test.java
+++ reflection/Test.java
@@ -3,13 +3,19 @@ import java.util.*;
 
 public class Test {
 	public static void main(String[] args) throws Exception{
-		Information information = new Information("/home/mr-fool/Documents/JFLAP.jar","org.apache.batik.bridge.PaintBridge");
+		Information information = new Information("/home/mr-fool/Documents/JFLAP.jar","org.apache.batik.transcoder.TranscoderInput");
 		information.printFunctions();
-		information.checkForFunction("fuck");
-		ArrayList<Integer> methodList = new ArrayList<Integer>();
-		methodList = information.properArguments("createPaint");
+		//information.checkForFunction("attributeIsFalse");
+		List<List<Integer>> methodList = new ArrayList<List<Integer>>(); 
+		methodList = information.properArguments("getURI");
 		for(Integer tmp: methodList) { 
 			System.out.println(tmp);
 		}//end-for
 	}//end main
 }
+/*javac Test.java
+Test.java:11: error: incompatible types: List<Integer> cannot be converted to Integer
+		for(Integer tmp: methodList) { 
+		                 ^
+1 error
+*/
