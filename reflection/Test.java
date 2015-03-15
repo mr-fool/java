79d0f102eb766801147deb3dd3ee6d58dc36468e
mr-fool <mr-fool@users.noreply.github.com>
2015-03-14 23:46:15 -0600

    Update Test.java

--- reflection/Test.java
+++ reflection/Test.java
@@ -3,19 +3,25 @@ import java.util.*;
 
 public class Test {
 	public static void main(String[] args) throws Exception{
+		int maxSize = 0;
 		Information information = new Information("/home/mr-fool/Documents/JFLAP.jar","org.apache.batik.transcoder.TranscoderInput");
 		information.printFunctions();
 		//information.checkForFunction("attributeIsFalse");
 		List<List<Integer>> methodList = new ArrayList<List<Integer>>(); 
 		methodList = information.properArguments("getURI");
-		for(Integer tmp: methodList) { 
+		/*for(Integer tmp: methodList) { 
 			System.out.println(tmp);
-		}//end-for
+		}*///end-for
+		for (int i = 0; i < maxSize; i++) {
+			for (List<Integer> innerList : methodList) {
+        //You don't need this if all lists are the same length.
+			if (i > innerList.size() - 1) { 
+				System.out.print("x ");
+		} else {
+			System.out.print(innerList.get(i) + " ");
+        }
+    }
+    System.out.println(); //new line for the next row
+}
 	}//end main
 }
-/*javac Test.java
-Test.java:11: error: incompatible types: List<Integer> cannot be converted to Integer
-		for(Integer tmp: methodList) { 
-		                 ^
-1 error
-*/
