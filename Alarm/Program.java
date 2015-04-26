7941139b0d14edad0bdecdfbb48a860d79b67199
mr-fool <mr-fool@users.noreply.github.com>
2015-04-25 21:44:45 -0600

    Update Program.java

--- Alarm/Program.java
+++ Alarm/Program.java
@@ -1,14 +1,19 @@
 /*
 javac -cp . alarm/Program.java then java -cp . alarm.Program
+Enter the time in the format of H:M and date in the format of d:m:y
 */
 package alarm;
 
 import java.io.IOException;
 
-public class Program {
+public class Program extends Thread{
+	public void run() {
+    }
 	public static void main(String[] args) throws IOException, InterruptedException{
 		Alarm alertClock = new Alarm();
-		alertClock.Alert("19:13");
+		String time = args[0];
+		String date = args[1];
+		alertClock.Alert(time,date);
 		
 	}
 }
