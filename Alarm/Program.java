a488717ff68ed32c7d011f2ee88d7400fd158648
mr-fool <mr-fool@users.noreply.github.com>
2015-04-26 11:57:50 -0600

    Update Program.java
    added music player and song selection support

--- Alarm/Program.java
+++ Alarm/Program.java
@@ -1,19 +1,20 @@
 /*
 javac -cp . alarm/Program.java then java -cp . alarm.Program
 Enter the time in the format of H:M and date in the format of d:m:y
+Usage java H:M d:m:y musicPlayer music
 */
 package alarm;
 
 import java.io.IOException;
 
-public class Program extends Thread{
-	public void run() {
-    }
+public class Program{
 	public static void main(String[] args) throws IOException, InterruptedException{
 		Alarm alertClock = new Alarm();
 		String time = args[0];
 		String date = args[1];
-		alertClock.Alert(time,date);
+		String player = args[2];
+		String music = args[3];
+		alertClock.Alert(time,date,player,music);
 		
 	}
 }
