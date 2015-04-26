7edfdd00b2085c57d68417543ed8a158f7fdf9a7
mr-fool <mr-fool@users.noreply.github.com>
2015-04-25 19:15:37 -0600

    Update Program.java

--- Alarm/Program.java
+++ Alarm/Program.java
@@ -1,9 +1,14 @@
+/*
+javac -cp . alarm/Program.java then java -cp . alarm.Program
+*/
 package alarm;
 
+import java.io.IOException;
+
 public class Program {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException, InterruptedException{
 		Alarm alertClock = new Alarm();
-		alertClock.Alert("16:53");
+		alertClock.Alert("19:13");
 		
 	}
 }
