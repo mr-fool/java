b2a2167242e8c492da2468f09c940371e0b79e7b
mr-fool <mr-fool@users.noreply.github.com>
2015-04-25 21:44:23 -0600

    Update Alarm.java
    using thread instead

--- Alarm/Alarm.java
+++ Alarm/Alarm.java
@@ -1,33 +1,19 @@
+/*
+javac -cp . alarm/Program.java then java -cp . alarm.Program
+Enter the time in the format of H:M and date in the format of d:m:y
+*/
 package alarm;
+
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
-public class Alarm {
-	//get current time
-	public Calendar Time(){
-		Calendar cal = Calendar.getInstance();
-    	cal.getTime();
-    	System.out.println("current time is :" + cal.getTime());
-		return cal;
-	}
-	public void Alert(String alarmTime) throws IOException, InterruptedException {
-		boolean waiting = true;
-		while(waiting == true ){
-			Calendar cal = Calendar.getInstance();
-			SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
-			if (timeFormat.format(cal.getTime()).equals(alarmTime) == false) {
-				cal = Calendar.getInstance();
-			}
-			else {
-				System.out.println("It's time to wake up");
-				ProcessBuilder p = new ProcessBuilder();
-				p.command("omxplayer", "/home/mr-fool/Music/Hotarubi.mp3");
-				p.start();
-
-				waiting = false;
-			}
-		}
-			
+public class Program extends Thread{
+	public void run() {
+    }
+	public static void main(String[] args) throws IOException, InterruptedException{
+		Alarm alertClock = new Alarm();
+		String time = args[0];
+		String date = args[1];
+		alertClock.Alert(time,date);
+		
 	}
 }
