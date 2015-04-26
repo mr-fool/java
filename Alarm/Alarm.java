74133fe3ddbe44aa9b92ca550f85f7f7f1b40124
mr-fool <mr-fool@users.noreply.github.com>
2015-04-25 19:04:13 -0600

    Update Alarm.java

--- Alarm/Alarm.java
+++ Alarm/Alarm.java
@@ -22,8 +22,9 @@ public class Alarm {
 			else {
 				System.out.println("It's time to wake up");
 				ProcessBuilder p = new ProcessBuilder();
-				p.command("/usr/bin/mplayer", "/home/mr-fool/Music/Running.mp3");
+				p.command("vlc", "/home/mr-fool/Music/Hotarubi.mp3");
 				p.start();
+
 				waiting = false;
 			}
 		}
